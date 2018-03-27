980b903275a366f8a582d0d3537958302c3e09fe
zouhecan <zouhecan@shu.edu.cn>
2018-03-27 11:23:50 +0800

    添加生命周期注释

--- app/src/main/java/com/example/zouhecan/eventBusDemo/MyFragment.java
+++ app/src/main/java/com/example/zouhecan/eventBusDemo/MyFragment.java
@@ -3,6 +3,7 @@ package com.example.zouhecan.eventBusDemo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import org.greenrobot.eventbus.ThreadMode;
 
 /**
  * desc: 本Fragment的数据没有通过Bundle传递，而是通过EventBus在Activity中发起post，fragment中接收，好处在于传递的对象数据不用序列化
+ * 生命周期: EventBus注册——>接收到EventBus数据——>onCreate——>onCreateView——>onCreateView——>onDestroy——>EventBus取消注册
  * @author zouhecan （zouhecan@58ganji.com）
  *         date: 2018/3/26
  */
@@ -26,6 +28,7 @@ public class MyFragment extends Fragment {
     public static MyFragment newInstance(){
         MyFragment fragment = new MyFragment();
         EventBus.getDefault().register(fragment);
+        Log.d(fragment.getClass().getName(), "EventBus注册");
         return fragment;
     }
 
@@ -36,10 +39,12 @@ public class MyFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(getClass().getName(), "fragment： onCreate");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d(getClass().getName(), "fragment： onCreateView");
         View view = inflater.inflate(R.layout.fragment_layout, container, false);
         priceView = view.findViewById(R.id.price);
         nameView = view.findViewById(R.id.name);
@@ -50,6 +55,7 @@ public class MyFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(getClass().getName(), "fragment： onActivityCreate");
         initView();
     }
 
@@ -61,10 +67,16 @@ public class MyFragment extends Fragment {
         }
     }
 
-
+    /**
+     * 根据生命周器，EventBus接收数据发生在View的实例化和绘制之前，那么如何完成刷新UI操作呢？
+     * 错误的做法：在接收数据的同事去刷新UI（因为此时view还没有绘制）
+     * 正确的做法：先接收数据，然后在onActivityCreated中，利用接收的数据去刷新UI
+     * @param shop
+     */
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEvent (MyShop shop) {
         if (shop != null) {
+            Log.d(getClass().getName(), "接收到EventBus数据");
             myShop = shop;
         }
     }
@@ -73,6 +85,8 @@ public class MyFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(getClass().getName(), "fragment： onDestroy");
         EventBus.getDefault().unregister(this);//取消注册
+        Log.d(getClass().getName(), "EventBus取消注册");
     }
 }
